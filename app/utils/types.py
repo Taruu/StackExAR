d00e391db61c093e9ab446a87de0e82bb3f4979b
taruu <taruumax@gmail.com>
2024-02-09 14:54:59 +0300

    archive reader done

--- app/utils/types.py
+++ app/utils/types.py
@@ -39,42 +39,53 @@ class DataArchive:
     reader = None
 
     @staticmethod
-    def _sync_read_archive(archive_path, filename, bytes_queue):
-        print(filename)
+    def _sync_read_archive_by_address(archive_path, filename, start: int, length: int):
         with SevenZipFile(archive_path, 'r') as archive_read:
-            file_reader = archive_read.read(targets=filename).get('Tags.xml')
+            file_reader = archive_read.read(targets=filename).get(filename)
+            file_reader.seek(start)
+            request_bytes = file_reader.read(length)
+        return request_bytes
+
+    @staticmethod
+    def _sync_read_archive(archive_path, filename, bytes_queue):
+        with SevenZipFile(archive_path, 'r') as archive_read:
+            file_reader = archive_read.read(targets=filename).get(filename)
+
             for line in file_reader.readlines():
-                # print(line)
                 bytes_queue.put_nowait(line)
         return True
 
-    async def async_read_archive(self, archive_path, filename):
+    async def async_read_archive_by_address(self, archive_path, filename, start: int, length: int):
+        loop = asyncio.get_running_loop()
+        return await loop.run_in_executor(global_app.app.process_pools, self._sync_read_archive_by_address,
+                                          archive_path, filename, start, length)
+
+    async def async_read_archive_generator(self, archive_path, filename):
         loop = asyncio.get_running_loop()
 
         m = multiprocessing.Manager()
         bytes_queue: multiprocessing.Queue = m.Queue()
-
-        temp_list = []
-
-        print("out", id(bytes_queue))
-
-        print("out", id(bytes_queue))
         sync_future: asyncio.Future = loop.run_in_executor(
             global_app.app.process_pools, self._sync_read_archive, archive_path, filename, bytes_queue)
 
-        # sync_task = asyncio.ensure_future(sync_future, loop=loop)
-
-        print(not bytes_queue.empty(), not sync_future.done())
         while (not bytes_queue.empty()) or (not sync_future.done()):
             try:
-                print("while", bytes_queue.get_nowait())
+                value_temp: bytes = bytes_queue.get_nowait()
+                yield value_temp
             except queue.Empty:
-                await asyncio.sleep(1)
+                await asyncio.sleep(0)
 
-        pass
+
 
     async def read_tags(self):
-        await self.async_read_archive(self.tags_archive_path, TAGS_FILENAME)
+        start_points = 0
+        async for line in self.async_read_archive_generator(self.tags_archive_path, TAGS_FILENAME):
+            print()
+            print("readlines", start_points, len(line), line)
+            print('by_add_read',
+                  await self.async_read_archive_by_address(self.tags_archive_path, TAGS_FILENAME, start_points,
+                                                           len(line)))
+            start_points += len(line)
 
     def __init__(self, archive_path: List[str] | str):
         if not is_7zfile(archive_path):
